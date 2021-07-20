fb8fe2bf822c9db7ec1c9b2ffe69f913feec8ccb
roman413-code <70152260+roman413-code@users.noreply.github.com>
2021-07-20 14:27:01 -0400

    added tiger logo to home page

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+import { findByLabelText } from '@testing-library/react';
 
 const useStyles = makeStyles((theme) => ({
     root: {
       marginRight: theme.spacing(1)
     },
+    box:{
+      display: 'flex',
+      justifyContent: 'center',
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    }
     
   }));
 
@@ -15,17 +23,20 @@ export default function Home(){
   return (
     <div className={classes.root}>
      
+      <img height='500' src='https://cdn.dribbble.com/users/719060/screenshots/2620048/tiger.jpg?compress=1&resize=800x600' /><br />
+      <div className={classes.box}>
       <Button 
       variant="contained"
       component={Link}
       to='/submissions'>Add Bullets
       </Button>
-      <br />
+      
       <Button 
       variant="contained"
       component={Link}
       to='/raters'>Rate Bullets
       </Button>
+      </div>
       
      
     </div>
